05d117910c591501200c9aec79ad910bde74f99c
Natsuki <qq973313484@gmail.com>
2024-08-12 17:02:46 +0900

    CheckBoxコンポーネントのAPIを作った

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,5 @@
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-      <highcharts :options="chartsOptions"></highcharts>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <highcharts :options="chartsOptions"></highcharts>
 </template>
 
 <script setup lang="ts">
---
+++ src/components/CheckBox.vue
@@ -0,0 +1,62 @@
+<template>
+  <div :class="$style.container" @click="onCheckBoxClicked">
+    <svg
+      v-if="checked === false"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <rect x="0.5" y="0.5" width="23" height="23" rx="3.5" fill="#F1F1F4" stroke="#D8D8DB" />
+    </svg>
+    <svg
+      v-else
+      :class="$style.checked"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <path
+        d="M21 6.99997L9 19L3.5 13.5L4.91 12.09L9 16.17L19.59 5.58997L21 6.99997Z"
+        fill="white"
+      />
+    </svg>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const props = defineProps<{
+  prefCode: number
+}>()
+
+const emits = defineEmits(['check', 'uncheck'])
+
+const checked = ref<boolean>(false)
+
+const onCheckBoxClicked = () => {
+  checked.value = !checked.value
+  if (checked.value) {
+    emits('check', props.prefCode)
+  } else {
+    emits('uncheck', props.prefCode)
+  }
+}
+</script>
+
+<style lang="scss" module>
+.container {
+  width: 24px;
+  height: 24px;
+  flex-shrink: 0;
+}
+
+.checked {
+  border-radius: 4px;
+  background: $color-primary;
+}
+</style>
