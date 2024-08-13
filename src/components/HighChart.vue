fadd6ce1239859063a1c94e278112592c2fd37a3
Natsuki <qq973313484@gmail.com>
2024-08-13 22:38:33 +0900

    feat: グラフ表示を実装した

--- src/components/HighChart.vue
+++ src/components/HighChart.vue
@@ -26,7 +26,29 @@ const seriesData = computed(() => {
   return res
 })
 
+const categoriesData = computed(() => {
+  let res: number[] = []
+  for (let year = 1960; year <= 2045; year += 5) {
+    res.push(year)
+  }
+  return res
+})
+
 const chartsOptions = ref({
+  title: {
+    text: '人口の推移'
+  },
+  xAxis: {
+    title: {
+      text: 'year'
+    },
+    categories: categoriesData
+  },
+  yAxis: {
+    title: {
+      text: 'population'
+    }
+  },
   series: seriesData
 })
 </script>
